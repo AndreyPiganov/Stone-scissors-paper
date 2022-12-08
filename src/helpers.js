57a6deb72219b778927856b9a1b0dec7b9d6ae39
AndreyPiganov <andrey.piganov@mail.ru>
2022-12-08 13:58:56 +0300

    update files

--- src/helpers.js
+++ src/helpers.js
@@ -16,5 +16,14 @@ const BotAnswer = () => {
   const getRandomElement = (min, max, arr) => arr[Math.floor(Math.random() * (max - min + 1)) + min];
   return getRandomElement(0, answers.length - 1, answers);
 };
+const startGame = () => {
+  console.log('Welcome to the stone,scissors,paper game!');
+  console.log(UserHello());
+  console.log('If you want to quit the game then write: exit or end.');
+  console.log('The game ends when one of the players reaches 2 wins.');
+  console.log('Lets start the game');
+};
 
-export { UserHello, UserAnswers, BotAnswer };
+export {
+  UserHello, UserAnswers, BotAnswer, startGame,
+};
