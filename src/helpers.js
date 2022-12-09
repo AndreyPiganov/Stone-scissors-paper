b00c7c850c557bc929b799ad2f1afc1235833ce8
AndreyPiganov <andrey.piganov@mail.ru>
2022-12-09 16:44:41 +0300

    fix files

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import GameData from './index.js';
 
 const UserHello = () => {
   const name = readlineSync.question('May I have your name? ');
@@ -23,6 +24,35 @@ const startGame = () => {
   console.log('The game ends when one of the players reaches 2 wins.');
   console.log('Lets start the game');
 };
+const LogicGame = (count) => {
+  console.log('To begin, write: stone, scissors, or paper.');
+  const text = `Your answer: ${UserAnswers()}`;
+  const text1 = `Bot answer: ${BotAnswer()}`;
+  const array = text.split(':');
+  const array1 = text1.split(':');
+  console.log(text);
+  console.log(text1);
+if (array[array.length - 1] === array1[array.length - 1]) {
+  console.log(`Draw!\nYour score: ${count}`);
+} else if (array[array.length - 1] === ' stone' && array1[array.length - 1] === ' scissors') {
+  count += 1;
+  console.log(`Victory!\nYour score: ${count}`);
+} else if (array[array.length - 1] === ' paper' && array1[array.length - 1] === ' stone') {
+  count += 1;
+  console.log(`Victory!\nYour score: ${count}`);
+} else if (array[array.length - 1] === ' scissors' && array1[array.length - 1] === ' paper') {
+  count += 1;
+  console.log(`Victory!\nYour score: ${count}`);
+} else if (array[array.length - 1] === ' exit') {
+  count += 3;
+} else if (array[array.length - 1] === ' end') {
+  count += 3;
+} else {
+  count += -1;
+  console.log(`You lose!\nYour score: ${count}`);
+}
+return GameData(count);
+}
 export {
-  UserHello, UserAnswers, BotAnswer, startGame
+  UserHello, UserAnswers, BotAnswer, startGame, LogicGame,
 };
--- src/index.js
+++ src/index.js
@@ -1,44 +1,19 @@
-import { UserAnswers, BotAnswer, startGame, } from './helpers.js';
+import { UserAnswers, BotAnswer, startGame, LogicGame } from './helpers.js';
 
 startGame();
+
 const GameData = (count) => {
-  console.log('To begin, write: stone, scissors, or paper.');
-  const text = `Your answer: ${UserAnswers()}`;
-  const text1 = `Bot answer: ${BotAnswer()}`;
-  const array = text.split(':');
-  const array1 = text1.split(':');
-  console.log(text);
-  console.log(text1);
-    if(count === 1 ){
-      console.log(`Your score: ${count+1}\nYou winner!!!\nGame over!`);
+    if(count === 2 ){
+      console.log('You winner!!!\nGame over!');
     }
-    else if(count === -1){
-      console.log(`Your score: ${count-1}\nYou loser!!!\nGame over!`);
+    else if(count === -2){
+      console.log('You loser!!!\nGame over!');
     }
     else if(count === 3){
       console.log('Good bye!');
     }
     else{
-      if (array[array.length - 1] === array1[array.length - 1]) {
-        console.log(`Draw!\nYour score: ${count}`);
-      } else if (array[array.length - 1] === ' stone' && array1[array.length - 1] === ' scissors') {
-        count += 1;
-        console.log(`Victory!\nYour score: ${count}`);
-      } else if (array[array.length - 1] === ' paper' && array1[array.length - 1] === ' stone') {
-        count += 1;
-        console.log(`Victory!\nYour score: ${count}`);
-      } else if (array[array.length - 1] === ' scissors' && array1[array.length - 1] === ' paper') {
-        count += 1;
-        console.log(`Victory!\nYour score: ${count}`);
-      } else if (array[array.length - 1] === ' exit') {
-        count += 3;
-      } else if (array[array.length - 1] === ' end') {
-        count += 3;
-      } else {
-        count += -1;
-        console.log(`You lose!\nYour score: ${count}`);
-      }
-      return GameData(count);
+      return LogicGame(count);
   }
 };
 export default GameData;
